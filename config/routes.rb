ae4a844985327d913d3ff73e8605ac1d9a0800cf
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-06-10 12:05:30 +0900

    create-sever-side

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root "messages#index"
+=======
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> b794352... create-server-side
 end
