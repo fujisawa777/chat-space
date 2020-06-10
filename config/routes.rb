fde53c74f38c439b7a00733f5384c6f9db4e289e
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-06-10 12:25:05 +0900

    commit-update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root "messages#index"
-=======
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> b794352... create-server-side
+  devise_for :users
+  root "messages#index"
 end
