ae5996aba701f2c9ce379cd10fb22eab3a734f01
fujisawa777 <66346129+fujisawa777@users.noreply.github.com>
2020-06-18 17:36:46 +0900

    修正

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,13 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
-      @group = Group.find(params[:group_id])
-      @message = Message.new
-      @messages = @group.messages.includes(:user)
-      respond_to do |format|
-        format.html
-        format.json{
-          @new_message = @messages.where('id > ?', params[:id])
-        }
-      end
+      # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
+    group = Group.find(params[:group_id])
+    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
+    last_message_id = params[:id]
+    # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
+    @messages = group.messages.includes(:user).where("id > ?", last_message_id)
   end
 end
